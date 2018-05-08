0a1a2a6968bf6d44a816ea3ad4f2139f7b921ef5
katalinkovacs <katalinkovacsnz@gmail.com>
2018-05-08 14:28:47 +1000

    Added more classes for logging

---
+++ Common-Library/src/main/java/com/kati/mylibrary/logger/StandardLogger.java
@@ -0,0 +1,78 @@
+package com.kati.mylibrary.beans.logger;
+
+import com.kati.mylibrary.beans.camel.StandardHeader;
+import org.apache.camel.Exchange;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.kati.mylibrary.beans.route.constants.Constants.STEP_FINISH;
+import static com.kati.mylibrary.beans.route.constants.Constants.STEP_START;
+
+
+public class StandardLogger {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StandardLogger.class);
+
+    private String defaultInterface_id;
+    private String defaultInterface_step;
+
+
+    public String getDefaultInterface_id() {
+        return defaultInterface_id;
+    }
+
+    public void setDefaultInterface_id(String defaultInterface_id) {
+        this.defaultInterface_id = defaultInterface_id;
+    }
+
+    public String getDefaultInterface_step() {
+        return defaultInterface_step;
+    }
+
+    public void setDefaultInterface_step(String defaultInterface_step) {
+        this.defaultInterface_step = defaultInterface_step;
+    }
+
+    public StandardLogger() {
+    }
+
+
+
+
+
+    public void logStart(Exchange exchange) throws Exception {
+
+        StandardHeader standardHeader = new StandardHeader();
+
+        standardHeader.setInterface_id(defaultInterface_id);
+        standardHeader.setInterface_step(defaultInterface_step);
+
+        exchange.getIn().setHeader("standardHeader", standardHeader);
+        log(exchange, STEP_START);
+    }
+
+
+
+
+    public void logFinished(Exchange exchange) throws Exception {
+        log(exchange, STEP_FINISH);
+    }
+
+
+
+
+
+
+    private void log(Exchange exchange, String stepName) throws Exception {
+
+        StandardHeader standardHeader = (StandardHeader) exchange.getIn().getHeader("standardHeader");
+        String standardheaderJson = standardHeader.toString();
+
+        LOG.info("StepName is: " +stepName + " The body of the message: " +  exchange.getIn().getBody().toString() + " The standard header is: " + standardheaderJson);
+
+    }
+
+
+
+
+}
---
+++ Common-Library/src/main/java/com/kati/mylibrary/route/constants/Constants.java
@@ -0,0 +1,17 @@
+package com.kati.mylibrary.beans.route.constants;
+
+
+public class Constants {
+
+    public static final String SIMPLE_BODY = "Simple_Body ${body}";
+    /*public static final String STEP_EXCEPTION = "Exception";*/
+    public static final String STEP_FINISH = "Step_Finish";
+    /*public static final String STEP_START_PROCESS = "Processing"; // Process <EntityName>*/
+    public static final String STEP_START = "Step_Start";
+
+
+
+    private Constants() { /* import statically rather than instantiate */ }
+
+
+}
